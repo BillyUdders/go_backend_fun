cc139ef0411bdabfeb1e7ffcc2d10b407107899d
BillyUdders <rhys.davies901@gmail.com>
2024-07-21 11:14:42 -0700

    Siccccck I figured it out this code is AWWESOMEEE

--- db.go
+++ db.go
@@ -7,6 +7,10 @@ import (
 	"reflect"
 )
 
+type RowScanner interface {
+	Scan(dest ...interface{}) error
+}
+
 func InitDB(driverName string, databaseName string, tableCreate string) *sql.DB {
 	val, err := sql.Open(driverName, databaseName)
 	if err != nil {
@@ -21,7 +25,26 @@ func InitDB(driverName string, databaseName string, tableCreate string) *sql.DB 
 
 func GetOne[T any](db *sql.DB, query string, params ...interface{}) (*T, error) {
 	row := db.QueryRow(query, params...)
+	return getOne[T](row)
+}
 
+func GetList[T any](db *sql.DB, query string, params ...interface{}) ([]*T, error) {
+	rows, err := db.Query(query, params...)
+	if err != nil {
+		return nil, err
+	}
+	var results []*T
+	for rows.Next() {
+		res, rowErr := getOne[T](rows)
+		if rowErr != nil {
+			return nil, rowErr
+		}
+		results = append(results, res)
+	}
+	return results, nil
+}
+
+func getOne[T any](scanner RowScanner) (*T, error) {
 	// Get the type of the generic parameter T
 	elemType := reflect.TypeOf((*T)(nil)).Elem()
 	elem := reflect.New(elemType).Elem()
@@ -34,7 +57,7 @@ func GetOne[T any](db *sql.DB, query string, params ...interface{}) (*T, error) 
 	}
 
 	// Scan the row into the column pointers
-	if err := row.Scan(columnPointers...); err != nil {
+	if err := scanner.Scan(columnPointers...); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return nil, nil
 		}
@@ -53,56 +76,4 @@ func GetOne[T any](db *sql.DB, query string, params ...interface{}) (*T, error) 
 	// Return the populated struct
 	result := elem.Addr().Interface().(*T)
 	return result, nil
-}
-
-func GetList[T any](db *sql.DB, query string) ([]T, error) {
-	rows, err := db.Query(query)
-	if err != nil {
-		return nil, err
-	}
-
-	// Get column names from the query result
-	columns, err := rows.Columns()
-	if err != nil {
-		return nil, err
-	}
-
-	// Prepare a slice to hold the results
-	var results []T
-
-	for rows.Next() {
-		// Create a new instance of type T
-		elemType := reflect.TypeOf((*T)(nil)).Elem()
-		elem := reflect.New(elemType).Elem()
-
-		// Create a slice of interfaces to hold the column values
-		columnValues := make([]interface{}, len(columns))
-		columnPointers := make([]interface{}, len(columns))
-		for i := range columnValues {
-			columnPointers[i] = &columnValues[i]
-		}
-
-		// Scan the row into the column pointers
-		if err = rows.Scan(columnPointers...); err != nil {
-			return nil, err
-		}
-
-		// Map the column values to the struct fields
-		for i := 0; i < elemType.NumField(); i++ {
-			field := elem.Field(i)
-			val := reflect.ValueOf(columnValues[i])
-			if val.Type().ConvertibleTo(field.Type()) {
-				field.Set(val.Convert(field.Type()))
-			}
-		}
-
-		// Append the result to the results slice
-		results = append(results, elem.Interface().(T))
-	}
-
-	if err = rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return results, nil
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module rhys_test.com/m
 go 1.22.5
 
 require github.com/mattn/go-sqlite3 v1.14.22
+
+require (
+	github.com/kisielk/gotool v1.0.0 // indirect
+	golang.org/x/tools v0.23.0 // indirect
+	honnef.co/go/implements v0.0.0-20160409101833-8d6e0e9067c0 // indirect
+)
